b50763a622100f9d1d23ea8543723f7feeba4888
SooDevv <soojung.dev@gmail.com>
2019-10-17 22:20:20 +0900

    add solution

---
+++ soojung-kim/94.py
@@ -0,0 +1,11 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
