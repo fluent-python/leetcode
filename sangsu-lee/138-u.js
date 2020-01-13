63a2d14ae500d2e3606f47e3b25f614912a40851
sangsu-lee <sangsu-lee@navercorp.com>
2020-01-14 01:23:58 +0900

    add 138-u.js

---
+++ sangsu-lee/138-u.js
@@ -0,0 +1,18 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+const copyRandomList = head => {
+  const nodeList = [];
+  while (node.next) {
+
+  }
+};
