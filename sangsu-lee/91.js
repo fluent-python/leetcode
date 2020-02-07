cd6266b5fb18a70428607d69a167b9ec0c3bf5b6
sangsu-lee <sangsu-lee@navercorp.com>
2020-02-08 08:54:06 +0900

    add 91.js

---
+++ sangsu-lee/91.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const numDecodings = (s) => {
+  if (s.length == 0 || s == null) return 0;
+  const dp = new Array(10000).fill(0);
+  dp[0] = 1; dp[1] = s[0] != 0 ? 1 : 0
+  for (let i = 2; i <= s.length; i++) {
+    let a = s.slice(i-1, i);
+    let b = s.slice(i-2, i);
+    if (a >= 1 && a <= 9) dp[i] += dp[i-1]
+    if (b >= 10 && b <= 26) dp[i] += dp[i-2]
+  }
+  return dp[s.length];
+}
